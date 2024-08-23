9728aef725ba8c36ca0f976e5bed91182def15cf
bishal1993 <maharjanbishal19@gmail.com>
2024-08-23 14:37:24 +0200

    Post.html file is created with bootstrap

--- springboot-blog-webapp/src/main/java/me/nepali/springboot/controller/PostController.java
+++ springboot-blog-webapp/src/main/java/me/nepali/springboot/controller/PostController.java
@@ -1,2 +1,30 @@
-package me.nepali.springboot.controller;public class PostController {
+package me.nepali.springboot.controller;
+
+import me.nepali.springboot.dto.PostDTO;
+import me.nepali.springboot.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class PostController {
+    private PostService postService;
+
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    //create handler method , GET Request and return model and view
+    @GetMapping("/admin/posts")
+    public String posts(Model model){
+        List<PostDTO> posts= postService.findAllPosts();
+        model.addAttribute("posts",posts);
+        return "/admin/posts"; // admin is folder
+    }
+
 }
--- springboot-blog-webapp/src/main/java/me/nepali/springboot/repository/PostRepository.java
+++ springboot-blog-webapp/src/main/java/me/nepali/springboot/repository/PostRepository.java
@@ -1,4 +1,11 @@
 package me.nepali.springboot.repository;
 
-public class PostRepository implements  Repository{
+import me.nepali.springboot.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostRepository extends JpaRepository<Post,Long> {
+    Optional<Post> findByUrl(String url);
+
 }
