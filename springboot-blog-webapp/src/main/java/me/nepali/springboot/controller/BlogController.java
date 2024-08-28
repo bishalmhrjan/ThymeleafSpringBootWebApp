83b5e66e9873ff1368dd2f775ea91f916050c265
bishal1993 <maharjanbishal19@gmail.com>
2024-08-28 21:27:12 +0200

    blog search is implemented

--- springboot-blog-webapp/src/main/java/me/nepali/springboot/controller/BlogController.java
+++ springboot-blog-webapp/src/main/java/me/nepali/springboot/controller/BlogController.java
@@ -3,9 +3,12 @@ package me.nepali.springboot.controller;
 import me.nepali.springboot.dto.PostDTO;
 import me.nepali.springboot.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -25,6 +28,21 @@ public class BlogController {
     List<PostDTO> postResponse = postService.findAllPosts();
     model.addAttribute("postsResponse",postResponse);
     return "blog/view-posts";
+    }
 
+    //handler method to handle view Post request
+
+    @GetMapping("/post/{postUrl}")
+    private String showPost(@PathVariable("postUrl") String postUrl, Model model){
+    PostDTO postDto = postService.finPostByUrl(postUrl);
+    model.addAttribute("post",postDto);
+    return "blog/blog-post";
+    }
+
+    //handler method to handle blog post search request
+    @GetMapping("/page/search") public String searchPosts(@RequestParam(value="query")String query, Model model){
+      List<PostDTO> postsResponse = postService.searchPosts(query);
+      model.addAttribute("postsResponse",postsResponse);
+      return "blog/view-posts";
     }
 }
